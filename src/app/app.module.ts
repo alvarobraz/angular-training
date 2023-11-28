f2b6e6862386c45dd9b8a743a6cd06fbb385940d
Álvaro Braz <alvarobraz83@gmail.com>
2023-11-28 15:27:09 -0300

    criação do header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { PagesModule } from './pages/pages.module';
+import { HeaderComponent } from './shared/header/header.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PagesModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
